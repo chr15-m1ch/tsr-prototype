7ef26b9189a8eaf097eec2aed98af5069b5a7278
chr15-m1ch <chris.surju@gmail.com>
2021-04-22 16:37:33 +0400

    updated

--- classify_realtime_roi.py
+++ classify_realtime_roi.py
@@ -1,69 +1,44 @@
 # penkor travay lor saaa!!
 # ena sa pu fr lerla merge
+# bzn train mo trsffic signe detector model avan
 
 # imports
 import numpy as np
 import cv2
-from tensorflow import keras
 from tensorflow.keras.models import load_model
-from skimage import transform
-from skimage import exposure
-from skimage import io
-from imutils import paths
 from tkinter import *
-from PIL import ImageTk, Image
-from tkinter import filedialog #for gui
-import pyttsx3 #for voice assistant
+# import pyttsx3 #for voice assistant
 
 # voice assistant parameters
-engine = pyttsx3.init() # object creation
-# RATE
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-engine.setProperty('rate', 200)     # setting up new voice rate
-# VOLUME
-volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-engine.setProperty('volume', 1.0)    # setting up volume level  between 0 and 1
-# VOICE
-voices = engine.getProperty('voices')       #getting details of current voice
-engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-# engine.say("Speed Limit 60 km/h")
-# engine.runAndWait()
-# engine.stop()
+# engine = pyttsx3.init() # object creation
+# # RATE
+# rate = engine.getProperty('rate')   # getting details of current speaking rate
+# engine.setProperty('rate', 200)     # setting up new voice rate
+# # VOLUME
+# volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+# engine.setProperty('volume', 1.0)    # setting up volume level  between 0 and 1
+# # VOICE
+# voices = engine.getProperty('voices')       #getting details of current voice
+# engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
 
-# GUI
-root = Tk()
-root.withdraw() #destroy the root window
 
-# select the video file
-# root.filename = filedialog.askopenfilename(
-#     initialdir="/TSR-UI/images", 
-#     title="Select a MP4 video", 
-#     filetypes=(("MP4 files", "*.mp4"), ("all files", "*.*")))
-
-# # print("file location: " + root.filename)
-# # get the location
-# filename = root.filename
 
 # probability threshold
 threshold = 0.5         
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-# load the model
-print("[INFO] loading model...")
+# load our models
+# print("[INFO] loading traffic sign detector model...")
+# model = load_model('')
+# print("[info] ...traffic sign detector model loaded")
+
+print("[INFO] loading classification model...")
 model = load_model('germanSubsetModel.p')
-
-# preprocessing
-def grayscale(img):
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    return img
-
-def equalize(img):
-    img =cv2.equalizeHist(img)
-    return img
+print("[info] ...classification model loaded")
 
 def preprocessing(img):
-    img = grayscale(img)
-    img = equalize(img)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img =cv2.equalizeHist(img)
     img = img/255
     return img
 
@@ -119,10 +94,7 @@ def voiceNotification(classNo):
         engine.stop()
 
 
-# read the video of the file chosen
-# cap = cv2.VideoCapture(filename) # use when choosing file
 cap = cv2.VideoCapture('videos/sample003.mp4')
-
 # cap = cv2.VideoCapture(0) #from webcam
 
 className = '' 
@@ -138,17 +110,29 @@ while(cap.isOpened()):
     
     # pre process the nth frame (but start with the first frame)
     if (frameCount == 0) or (frameCount%10==0) :
-        # cv2.imshow('frame',frame)
+
+        # detect traffic signs
+        # extrack ROI
+        # .
+        # ################### codes here
+        # .
+        # .
+
+        # then process the ROI and pass it through the network
+        # .
+        # .
+        # ..
+
         # process the frame
-        img = np.asarray(imgOrignal)
-        img = cv2.resize(img, (32, 32))
-        img = preprocessing(img)
+        roi = np.asarray(imgOrignal)
+        roi = cv2.resize(roi, (32, 32))
+        roi = preprocessing(roi)
         # cv2.imshow("Processed Image", img)
-        img = img.reshape(1, 32, 32, 1)
+        roi = roi.reshape(1, 32, 32, 1)
 
         # predict frame
-        predictions = model.predict(img)
-        classIndex = model.predict_classes(img)
+        predictions = model.predict(roi)
+        classIndex = model.predict_classes(roi)
         probabilityValue =np.amax(predictions)
 
         p = probabilityValue*100
@@ -159,9 +143,6 @@ while(cap.isOpened()):
             cv2.putText(display, "SIGN CLASS: " , (20, 35), font, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.putText(display, "PROBABILITY: ", (20, 75), font, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
 
-
-            # if probabilityValue > threshold:
-                #print(getClassName(classIndex))
             className = str(getClassName(classIndex))
             print (className)
             cv2.putText(display, className, (180, 35), font, 0.75, (0, 255, 0), 2, cv2.LINE_AA)
@@ -172,18 +153,12 @@ while(cap.isOpened()):
                 pColor = (0,255,0) #make prob appear green if probability is greater than 80%
 
             cv2.putText(display, str(round(probabilityValue*100,2) )+"%", (180, 75), font, 0.75, pColor, 2, cv2.LINE_AA)
-        
-            # print(int(classIndex))
-            # print(dummyIndex)
-            # print(dummyIndex != int(classIndex))
 
-
-            #trigger voice assitant when confidence > 90 and index from last frame is not same as current frame
-            if (dummyIndex != int(classIndex) and p > 90):
-                print("here")
-                dummyIndex = classIndex
-                voiceNotification(classIndex)
-            # voice assistant
+            # #trigger voice assitant when confidence > 90 and index from last frame is not same as current frame
+            # if (dummyIndex != int(classIndex) and p > 90):
+            #     print("here")
+            #     dummyIndex = classIndex
+            #     voiceNotification(classIndex)
 
         cv2.imshow("Video classification", display)
     
